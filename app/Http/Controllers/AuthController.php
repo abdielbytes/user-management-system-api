fa4720dc1374afea6077c21b6dfb35f62d5f4f45
abdielbytes <wilsonabdiel86@gmail.com>
2024-12-12 21:24:47 +0100

    passport nstalled

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        dd($request->all());
+        $validate = Validator::make([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => '<PASSWORD>',
+
+        ]);
+    }
 }
