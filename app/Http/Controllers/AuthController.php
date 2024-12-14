0710f26765362428ebc395a419de11b620a3f98f
abdielbytes <wilsonabdiel86@gmail.com>
2024-12-14 03:08:06 +0100

    wip

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,18 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        dd($request->all());
-        $validate = Validator::make([
-            'name' => 'required',
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        $token = $user->createToken($request->email)->accessToken;
+        return response()->json([
+            'message' => 'success',
+            'user'=> $user,
+            'token'=> $token
+            ]);
+    }
+
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => '<PASSWORD>',
-
         ]);
+        if ($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+
     }
 }
