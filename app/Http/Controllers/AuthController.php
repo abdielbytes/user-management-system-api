3b94e00cf7580b49e104d2c8ee466c70342dc3a3
abdielbytes <wilsonabdiel86@gmail.com>
2024-12-14 03:20:01 +0100

    login and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -43,7 +45,68 @@ class AuthController extends Controller
         if ($validator->fails()){
             return response()->json($validator->errors(), 422);
         }
+        try {
+            DB::beginTransaction();
+            $user = User::where('email', $request->email)->first();
+
+            if (Auth::attempt($request->only('email', 'password'))) {
+                $user->tokens->each(function ($token) {
+                    $token->delete();
+                });
+
+     
+
+                $token = $user->createToken($request->email)->accessToken;
+                $user->last_activity = now();
+
+                $user->save();
+                DB::commit();
+                return response()->json([
+                    'success' => true,
+                    "userData" => $user,
+                    'message' => 'Login successful',
+                    'token' => $token,
+                    'token_type' => 'Bearer',
+                ], 200);
+            }
+
+            $userEmailExist = User::where('email', $request->email)->exists();
+
+            DB::commit();
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid login credentials',
+            ], 401);
+        } catch (\Exception $e) {
+
+            DB::rollBack();
 
 
+            return response()->json([
+                'success' => false,
+                'message' => 'Something went wrong. Please try again.',
+            ], 500);
+        }
+
+
+    }
+    public function logout(Request $request)
+    {
+        $user = Auth::user();
+
+        if ($user){
+            $user->token->each(function($token){
+                $token->delete();
+            });
+        
+            return response()->json([
+                'message' => 'Successfully logged out',
+                'success' => true,
+            ], 200);
+        }
+        return response()->json([
+            'message'=>'User not authenticated',
+            'succeess' => false,
+        ], 401);
     }
 }
