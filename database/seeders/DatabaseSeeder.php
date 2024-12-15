e606c3f998c879bc386b2c463627bcfaa003b3ee
abdielbytes <wilsonabdiel86@gmail.com>
2024-12-15 06:04:58 +0100

    user controller tests

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin User',
             'email' => 'admin@example.com',
-            'password' => bcrypt('password'),
+//            'password' => bcrypt('password'),
         ]);
     }
 }
