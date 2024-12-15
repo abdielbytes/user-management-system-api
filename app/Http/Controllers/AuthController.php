9e3afe9711ef4601df2d24239093ec26adedc212
abdielbytes <wilsonabdiel86@gmail.com>
2024-12-15 07:12:33 +0100

    copied new project

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('UserToken')->accessToken;
+
+        return response()->json([
+            'message' => 'success',
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function login(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid login credentials',
+            ], 401);
+        }
+
+        // Revoke existing tokens
+        $user->tokens->each(function ($token) {
+            $token->delete();
+        });
+
+        // Create a new token
+        $token = $user->createToken('Personal Access Token')->accessToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login successful',
+            'token' => $token,
+            'token_type' => 'Bearer',
+        ], 200);
+    }
+
+ 
+
+    public function logout(Request $request)
+    {
+        if (Auth::user()) {
+            $user = Auth::user()->token();
+            $user->revoke();
+
+            return response()->json([
+                'message' => 'Successfully logged out',
+                'success' => true,
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'User not authenticated',
+            'success' => false,
+        ], 401);
+    }
+
+}
